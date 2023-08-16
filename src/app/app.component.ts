a49860c67bb8bfaa4a247e014f3f804679053346
shaikakhila123 <139138283+shaikakhila123@users.noreply.github.com>
2023-08-16 20:49:52 +0530

    Assignment-6

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, ViewChild } from '@angular/core';
+
+import {NgForm} from '@angular/forms'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  subscriptions=['Basic','Advanced','Pro'];
+  selectedSubscription='Advanced';
+  @ViewChild('signUpForm')
+  sgnForm!: NgForm;
+
+  onSubmit(){
+   console.log(this.sgnForm.value);
+  }
+}
